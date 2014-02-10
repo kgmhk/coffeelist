102033bc3cd584b9e18399e3335f69f4afcd3816
gihyun <gihyunkwak@naver.com>
2014-02-10 20:34:08 +0900

    2014-02-10

---
+++ CoffeList/src/com/example/coffelist/activity/ExpandableListViewAngel.java
@@ -0,0 +1,382 @@
+package com.example.coffelist.activity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+import com.example.coffelist.R;
+import com.example.coffelist.adapter.BaseExpandableAdapter;
+
+
+public class ExpandableListViewAngel extends Activity implements LocationListener{
+	
+	//location
+	private LocationManager locManager;
+	Geocoder geoCoder;
+	private Location myLocation = null;
+	double latPoint = 0;
+	double lngPoint = 0;
+	float  speed = 0;
+	
+	
+	private ArrayList<String> mGroupList = null;
+	private ArrayList<ArrayList<String>> mChildList = null;
+	private ArrayList<String> mChildListContent = null;
+	private ArrayList<String> mChildListContent2 = null;
+	private ArrayList<String> mChildListContent3 = null;
+	private ArrayList<String> mChildListContent4 = null;
+	private ArrayList<String> mChildListContent5 = null;
+	private ArrayList<String> mChildListContent6 = null;
+	private ArrayList<String> mChildListContent7 = null;
+	private ArrayList<String> mChildListContent8 = null;
+	private ArrayList<String> mChildListContent9 = null;
+	private ArrayList<String> mChildListContent10 = null;
+	
+	
+	//계산???�어�?TextView
+	private TextView coffename;
+	private TextView coffeprice;
+	private TextView coffepriceavr;
+	private EditText dutchcal;
+	private String total;
+	private int totalint;
+	public int check;
+	private View scateList;
+	private double totalavr;
+	private String totalstring = "";
+	private String name [][] = {{"더치 아메리카노","아메리카노","에스프레소","드립커피","더치라떼","카라멜마끼아또","화이트초코카페모카","헤이즐넛카페모카","카페모카",
+		"메이플마끼아또","카라멜카페모카","바닐라카라멜마끼아또","바닐라카페라떼","카페라떼/카푸치노"},{"쿠키&크림 스노우","자바 초콜릿칩 스노우","라임 버블 스노우","망고 버블 스노우","코코넛 버블 스노우","그린티 스노우","리얼 사과 스노우",
+		"리얼 바나나 스노우","딸기 스노우","카라멜 모카 스노우","모카 스노우","레몬 요거트 스노우","블루베리 스노우","요거트 스노우","자몽 스노우","골드 키위 스노우"},{"더치 라떼",
+			"후레쉬주스(토마토/키위)","리얼다크초코58%","핫초코/아이스초코","레모네이드","오렌지주스","그린티 라떼","티 라떼(블랙티/마테/루이보스)","아이스 티",
+			"스트로베리 티","국화차/녹차/아쌈/다즐링","루이보스/레드오렌지","레몬티"}};
+	
+	private int price [][] = {{5000,3900,3900,3500,5500,5400,5300,5200,5100,5000,5000,5000,4900,4400},{6100,6100,6100,6100,6100,600,6000,6000,5600,5500,5500,5500,5500,5500,5300,5200},{6000,5200,4500,
+		3900,3800,5500,5300,5300,4000,4500,4000,4000,3800}};
+			
+	//계산?�서 초기??버튼
+	private Button b1;
+	private Button calbutton;
+	private Button map;
+	private Button movebutton;
+	private Button young;
+	
+		SharedPreferences setting;
+	SharedPreferences.Editor editor;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//?�이?�웃 겹치�?		
+		Window win = getWindow();
+		win.setContentView(R.layout.list_main); //�?번째 메인 ?�이?�웃
+		
+		LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.overlaybutton, null);
+		
+		LinearLayout.LayoutParams paramlinear = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+		win.addContentView(linear, paramlinear);
+		
+		//setContentView(R.layout.list_main);
+		setLayout();
+		
+		
+		//location ?�정
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+       // locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, this);
+        locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 5, this);
+ 
+        geoCoder = new Geocoder(this, Locale.KOREAN); 
+		
+		
+		setting = getSharedPreferences("setting", 0);
+		editor = setting.edit();
+		//fragment??값넣�?TextView?�이??�?�?��?�기
+		coffename = (TextView) findViewById(R.id.coffename);
+		coffeprice = (TextView) findViewById(R.id.coffeprice);
+		coffepriceavr = (TextView) findViewById(R.id.coffepriceavr);
+		b1 = (Button) findViewById(R.id.inital);
+		calbutton = (Button) findViewById(R.id.calbutton);
+		dutchcal = (EditText)findViewById(R.id.dutchcal);
+		map = (Button) findViewById(R.id.map);
+		movebutton = (Button) findViewById(R.id.overcalbutton);
+		young = (Button) findViewById(R.id.youngyaung);
+		
+		
+		// ExpandableListView ?�정
+		mGroupList = new ArrayList<String>();
+		mChildList = new ArrayList<ArrayList<String>>();
+		mChildListContent = new ArrayList<String>(); 
+		mChildListContent2 = new ArrayList<String>();
+		mChildListContent3 = new ArrayList<String>();
+		mChildListContent4 = new ArrayList<String>();
+		mChildListContent5 = new ArrayList<String>();
+		mChildListContent6 = new ArrayList<String>();
+		mChildListContent7 = new ArrayList<String>();
+		mChildListContent8 = new ArrayList<String>();
+		mChildListContent9 = new ArrayList<String>();
+		mChildListContent10 = new ArrayList<String>();
+
+		mGroupList.add("에스프레소 음료");
+		mGroupList.add("엔젤린-스노우");
+		mGroupList.add("주스＇티＇음료");
+
+		//�?번째 그룹??차일??		
+		for(int i=0; i<name[0].length; i++){
+			mChildListContent.add(name[0][i] + "\n" + price[0][i] + "원");
+		}
+		Log.d("name1", String.valueOf(name[0].length));
+		Log.d("name1", String.valueOf(price[0].length));
+		//??번째 그룹??차일??		
+		for(int i=0; i<name[1].length; i++){
+			mChildListContent2.add(name[1][i] + "\n" + price[1][i] + "원");
+		}
+		
+		Log.d("name2", String.valueOf(name[1].length));
+		Log.d("name2", String.valueOf(price[1].length));
+		
+		//?�번�?그룹??차일??		
+		for(int i=0; i<name[2].length; i++){
+			mChildListContent3.add(name[2][i] + "\n" + price[2][i] + "원");
+		}
+		Log.d("name3", String.valueOf(name[2].length));
+		Log.d("name3", String.valueOf(price[2].length));
+		//??번째 차일??		
+				
+		
+		mChildList.add(mChildListContent);
+		mChildList.add(mChildListContent2);
+		mChildList.add(mChildListContent3);
+		
+		
+		mListView.setAdapter(new BaseExpandableAdapter(this, mGroupList, mChildList));
+		
+		// dutch Text Setting
+		//dutch.setText("?�치 계산");
+		
+		
+	      
+		
+		// 계산 버튼 ?�릭 ??		
+		calbutton.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				//coffepriceavr.setText(inwonstr);
+				int inwon = Integer.parseInt(dutchcal.getText().toString());
+				if(inwon == 0){
+					coffepriceavr.setText("인원을 입력 하세요.");
+				}
+				else{
+				totalavr = totalint / inwon;
+				String avrstr = Integer.toString((int)totalavr);
+				coffepriceavr.setText("---------------\n" +"총 인원 : " + inwon + "명\n" + "1  인당: " +avrstr +"원");
+				}
+			}
+		});
+	
+		// 계산�?fragment move
+		movebutton.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				chgLayoutDisplay();
+			}
+		});
+		
+		//young.setVisibility(View.VISIBLE);
+		// ?�양 ?�보 버튼
+		young.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				Uri u;
+				u = Uri.parse("http://www.caffebene.co.kr/sub04/nutritionFacts/facts01");
+				intent.setData(u);
+				startActivity(intent);
+				
+			}
+		});
+		
+		
+		map.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				Log.d("button click", "button");
+				GetLocations();
+				
+				
+			}
+		});
+		
+
+		
+		
+		//초기??버튼 ?�릭 ??		
+		b1.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v){
+				dutchcal.setText("0");
+				totalint = 0;
+				totalstring = "";
+				total = "";				
+				coffename.setText("");
+				coffepriceavr.setText("");
+				coffeprice.setText("");
+			}
+		});
+		
+		// 그륩???�릭?�을 ??
+		mListView.setOnGroupClickListener(new OnGroupClickListener() {
+			//@Override
+			public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+				Toast.makeText(getApplicationContext(), "g click = " + groupPosition, 
+						Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		});
+		
+		// 차일???�릭 ?�을 ??
+		mListView.setOnChildClickListener(new OnChildClickListener() {
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+				Toast.makeText(getApplicationContext(), "c click = " + childPosition, 
+						Toast.LENGTH_SHORT).show();
+				
+				//coffepriceavr.setText(Integer.toString(cnt) + "??);
+				totalstring += "◎" + name[groupPosition][childPosition] + "\n";
+				totalint += price[groupPosition][childPosition];
+				total = Integer.toString(totalint);
+				coffename.setText(totalstring);
+				coffeprice.setText("총 금액 : " + total +"원");
+				//mListView = (ExpandableListView) findViewById(R.id.elv_list);
+				return false;
+			}
+		});
+		
+		// 그룹???�힐 경우	
+		mListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+			@Override
+			public void onGroupCollapse(int groupPosition) {
+				Toast.makeText(getApplicationContext(), "g Collapse = " + groupPosition, 
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		// 그룹???�릴 경우		
+		mListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+			@Override
+			public void onGroupExpand(int groupPosition) {
+				Toast.makeText(getApplicationContext(), "g Expand = " + groupPosition, 
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+	public void chgLayoutDisplay(){
+		scateList = (View)findViewById(R.id.elv_list);
+		LinearLayout.LayoutParams p = (android.widget.LinearLayout.LayoutParams) scateList.getLayoutParams();
+		
+		int h = p.width;
+		int i = 0;
+		if(h < 500){
+			p.width = 720;
+			scateList.setLayoutParams(p);
+		}else{
+			Log.d("p.width", String.valueOf(p.width));
+			//p.width -= 0.000005;
+			p.width = 435;
+			scateList.setLayoutParams(p);
+		}
+	}
+
+	public void GetLocations() {
+		
+		StringBuffer juso = new StringBuffer();
+
+		Log.d("getlocation in", "in");
+		
+		if (myLocation != null) {
+			latPoint = myLocation.getLatitude();
+			lngPoint = myLocation.getLongitude();
+			Log.d("latPoint", String.valueOf(latPoint));
+			//speed = (float)(myLocation.getSpeed() * 3.6);
+		try {
+			Log.d("juso in", String.valueOf(latPoint));
+			Log.d("juso in", String.valueOf(lngPoint));
+				// �꾨�?寃쎈룄瑜��?�슜�섏�?�꾩??�꾩?���二?�냼?��媛�졇�?�떎. 
+				List<Address> addresses;
+				addresses = geoCoder.getFromLocation(latPoint, lngPoint, 1);
+				Log.d("addresses after", String.valueOf(lngPoint));
+				for(Address addr: addresses){
+					Log.d("for in", String.valueOf(lngPoint));
+					int index = addr.getMaxAddressLineIndex();
+					for(int i=0;i<=index;i++){
+						Log.d("2:for in", String.valueOf(lngPoint));
+						juso.append(addr.getAddressLine(i));
+						juso.append(" ");
+					}
+					Log.d("1:for end", String.valueOf(lngPoint));
+					juso.append("\n");
+				}
+				Log.d("2:for end", String.valueOf(lngPoint));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String juso1 = String.valueOf(juso);
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			Uri u;
+			u = Uri.parse("https://maps.google.co.kr/?q=엔젤리너스&near=" + juso1);
+			intent.setData(u);
+			startActivity(intent);
+				
+		}		
+			
+	}
+
+	public void onLocationChanged(Location location) {
+		Log.d("location", "location changed");
+		myLocation = location;
+	}
+
+	public void onProviderDisabled(String s) {
+
+	}
+
+	public void onProviderEnabled(String s) {
+
+	}
+
+	public void onStatusChanged(String s, int i, Bundle bundle) {
+
+	}
+
+	/*
+	 * Layout
+	 */
+	private ExpandableListView mListView;
+
+	private void setLayout(){
+		mListView = (ExpandableListView) findViewById(R.id.elv_list);
+	}
+}
